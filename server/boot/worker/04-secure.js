008239299157a195500679903c4e8b6c0a64dde4
Thomas Wilkins <twilkins@radialspark.com>
2018-07-09 09:46:34 -0700

    Adds in proxy trust to mitigate heroku internal routing

--- server/boot/worker/04-secure.js
+++ server/boot/worker/04-secure.js
@@ -5,6 +5,7 @@ const secure = require(`${PROJECT_DIR}/server/middleware/secure`);
 
 module.exports = (server) => {
   if (process.env.NODE_ENV === 'production') {
+    server.enable('trust proxy');
     server.use(secure);
   }
 }
