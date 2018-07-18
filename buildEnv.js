ff3784ce8d0c7c3ba9ac64fbb1a0240f4eb9f315
wilkins88 <twilkins@radialspark.com>
2018-07-18 11:33:41 -0700

    Adds in env var loading for build script (#14)

--- buildEnv.js
+++ buildEnv.js
@@ -1,14 +1,17 @@
+require('dotenv').config();
+
 const fs = require('fs');
 const path = require('path');
 
-
 const environmentFilesDirectory = path.join(__dirname, './src/environments');
-const targetEnvironmentFileName = 'environment.ts';
+const targetEnvironmentFileName = 'environment.prod.ts';
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 const isProduction = NODE_ENV === 'production' ? true : false;
 let apiUrl;
+
+// change these for each application
 switch (NODE_ENV) {
   case 'production':
     apiURL = 'http://localhost:5000/api';
@@ -21,7 +24,7 @@ switch (NODE_ENV) {
     break;
 }
 
-const template = `export const environment = { production: ${isProduction}, apiUrl: '${apiUrl}' }`;
+const template = `export const environment = { production: ${isProduction}, apiUrl: '${apiUrl}' };`;
 
 // Write environment file
 fs.writeFileSync(path.join(environmentFilesDirectory, targetEnvironmentFileName), template);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,1 @@
-export const environment = {
-  production: true
-};
+export const environment = { production: false, apiUrl: 'http://localhost:5000/api' };
